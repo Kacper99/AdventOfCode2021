e5527d7480789ad7f74747aace8590304ad8dad6
Kacper Martela <kacper99@hotmail.co.uk>
2021-12-23 23:10:28 +0000

    Part 1.2

--- src/main/kotlin/1-SonarSweep.kt
+++ src/main/kotlin/1-SonarSweep.kt
@@ -2,9 +2,18 @@ import java.io.File
 
 fun main(args: Array<String>) {
     val measurements = File("src/main/resources/input1.txt").readLines().map { it.toInt() }
+
+    // Part 1
     var increases = 0
     for (i in 1 until measurements.size) {
         if (measurements[i] > measurements[i - 1]) increases++
+    }
+    println(increases)
+
+    // Part 2
+    increases = 0
+    for (i in 4..measurements.size) {
+        if (measurements.subList(i - 3, i).sum() > measurements.subList(i - 4, i - 1).sum()) increases++
     }
     println(increases)
 }
