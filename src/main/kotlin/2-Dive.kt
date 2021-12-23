87117c77cf6b5eb0a24e4d4e8ed673c9a9a1d095
Kacper Martela <kacper99@hotmail.co.uk>
2021-12-23 23:23:25 +0000

    Part 2.2

--- src/main/kotlin/2-Dive.kt
+++ src/main/kotlin/2-Dive.kt
@@ -1,14 +1,37 @@
 import java.io.File
 
 fun main() {
+    part1()
+    part2()
+}
+
+fun part1() {
     var pos = 0
     var depth = 0
     File("src/main/resources/input2.txt").forEachLine {
-        var (action, amount) = it.split(" ")
+        val (action, amount) = it.split(" ")
         when (action) {
             "forward" -> pos += amount.toInt()
             "up" -> depth -= amount.toInt()
             "down" -> depth += amount.toInt()
+        }
+    }
+    println(pos * depth)
+}
+
+fun part2() {
+    var pos = 0
+    var depth = 0
+    var aim = 0
+    File("src/main/resources/input2.txt").forEachLine {
+        val (action, amount) = it.split(" ")
+        when (action) {
+            "forward" -> {
+                pos += amount.toInt()
+                depth += amount.toInt() * aim
+            }
+            "up" -> aim -= amount.toInt()
+            "down" -> aim += amount.toInt()
         }
     }
     println(pos * depth)
